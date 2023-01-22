9f75be0a25407e7ca49b652d35309e7e11f484fa
Fecnomz <44179107+Fecnomz@users.noreply.github.com>
2023-01-22 00:20:12 -0500

    fixed andrew mistake

--- front-end/client.js
+++ front-end/client.js
@@ -5,7 +5,7 @@ function submitGenerate(length,uppercase,symbols,numbers){
     console.log("kss ert el dans le client")
     socket.emit("Generate", length,uppercase,symbols,numbers, (response) => {
         var arrayPassword = response.status.split(',');
-        console.log(arrayPassword);
+        arrayPassword.toString();
     });
 
 }

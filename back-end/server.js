8a17a33a3e1bd4df7eae897abd3623514beff4d6
Fecnomz <44179107+Fecnomz@users.noreply.github.com>
2023-01-21 23:39:01 -0500

    server

--- back-end/server.js
+++ back-end/server.js
@@ -44,7 +44,6 @@ async function generatePassword(length,uppercase,symbols,numbers) {
     temperature: 0.7,
     max_tokens: 256,
   });
-  // response = gpt["data"]["choices"][0]["text"]
   response = gpt.data.choices[0].text;
   console.log(response);
   return response;

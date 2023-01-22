14f2c46d5a25e601718c7ed9f11a6cd70d1bc334
Fecnomz <44179107+Fecnomz@users.noreply.github.com>
2023-01-22 01:38:02 -0500

    added loading

--- front-end/client.js
+++ front-end/client.js
@@ -2,7 +2,8 @@ const socket = io("http://137.184.74.124:3000");
 
 
 function submitGenerate(length,uppercase,symbols,numbers){
-    console.log("kss ert el dans le client")
+    document.getElementById("output").innerHTML = "";
+    document.getElementById("output").innerHTML = "Loading...";
     socket.emit("Generate", length,uppercase,symbols,numbers, (response) => {
         var arrayPassword = response.status.split(',');
         document.getElementById("output").innerHTML = "";

72e99395e1632a40664decdeade141fbc1027b41
killianneer <daniel.mansour02@gmail.com>
2023-01-21 22:20:00 -0500

    client + server modification

--- front-end/client.js
+++ front-end/client.js
@@ -4,7 +4,8 @@ const socket = io("http://137.184.74.124:3000");
 function submitGenerate(length,uppercase,symbols,numbers){
     console.log("kss ert el dans le client")
     socket.emit("Generate", length,uppercase,symbols,numbers, (response) => {
-        console.log(response);
+        var arrayPassword = response.split(',');
+        console.log(arrayPassword);
     });
 
 }

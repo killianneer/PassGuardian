3cf4954a19846c90159a127fc41abd3dd2b2cbc0
Fecnomz <44179107+Fecnomz@users.noreply.github.com>
2023-01-22 01:31:33 -0500

    fixed bug reported by mansourati

--- front-end/client.js
+++ front-end/client.js
@@ -5,6 +5,7 @@ function submitGenerate(length,uppercase,symbols,numbers){
     console.log("kss ert el dans le client")
     socket.emit("Generate", length,uppercase,symbols,numbers, (response) => {
         var arrayPassword = response.status.split(',');
+        document.getElementById("output").innerHTML = "";
         for (var i = 0; i < arrayPassword.length; i++) {
             var paragraph = document.createElement("p");
             var text = document.createTextNode(arrayPassword[i]);
